baf2f4234bb8899aac48f78501b6f30635e801d4
Richi <richipineda2@gmail.com>
2023-06-27 00:18:43 +0100

    new data added, plus new types added for that data

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -1,4 +1,4 @@
-import patients from '../../data/patients';
+import patients from '../../data/patients-full';
 import { PatientEntry, NonSensitivePatientEntry, NewPatientEntry } from '../types';
 import { v4 as uuidv4 } from 'uuid';
 
--- src/types.ts
+++ src/types.ts
@@ -27,9 +27,43 @@ export interface DiagnoseEntry {
   latin?: string;
 };
 
-export interface Entry {
-
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry['code']>;
 }
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface PatientEntry {
   id: string;
@@ -45,4 +79,5 @@ export type NewPatientEntry = Omit<PatientEntry, 'id'>;
 export enum Gender {
   Male = 'male',
   Female = 'female',
+  Other = "Other"
 };

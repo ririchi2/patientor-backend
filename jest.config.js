b270c6756fff4b928a8c41fcd8acf4ca61394456
Richi <richipineda2@gmail.com>
2023-10-25 12:56:15 +0100

    installed jest and supertest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleNameMapper: {
+    // Add any module mappings if needed
+  },
+};

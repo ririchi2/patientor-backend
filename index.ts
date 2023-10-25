d34a3afa217b0f5185511333e0fbd0ff3e5e4e4a
Richi <richipineda2@gmail.com>
2023-10-25 12:55:26 +0100

    removed log and exported express app

--- index.ts
+++ index.ts
@@ -4,14 +4,13 @@ import diaryRouter from './src/routes/diaries';
 import diagnoseRouter from './src/routes/diagnoses';
 import patientRouter from './src/routes/patients';
 
-const app = express();
+export const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
   res.send('pong');
 });
 

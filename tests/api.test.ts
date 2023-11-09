168c9014150ec980e1aa085b29ddd24220d0feea
Richi <richipineda2@gmail.com>
2023-11-09 09:40:33 +0000

    rearranged tests in each describe

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -10,7 +10,7 @@ describe('GET /api/ping', () => {
   })
 })
 
-describe('GET /api/diaries', () => {
+describe('/api/diaries', () => {
   test('/api/diaries returns 200 and nonempty json', async () => {
     const response = await supertest(app).get('/api/diaries');
 
@@ -18,14 +18,43 @@ describe('GET /api/diaries', () => {
     expect(response.type).toBe('application/json');
     expect(response.body.length).toBeGreaterThan(0)
   })
-})
-
-describe('GET /api/diaries/:id', () => {
   test('/api/diaries/:id returns 200 and nonempty json', async () => {
     const response = await supertest(app).get('/api/diaries/1');
 
     expect(response.status).toBe(200);
     expect(response.type).toBe('application/json');
     expect(Object.keys(response.body).length).toBeGreaterThan(0)
+  })
+})
+
+describe('/api/diagnoses', () => {
+  test('/api/diagnoses returns 200 and nonempty json', async () => {
+    const response = await supertest(app).get('/api/diagnoses');
+
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body.length).toBeGreaterThan(0)
+  })
+})
+
+describe('/api/patients', () => {
+  test('/api/patients returns 200 and nonempty json', async () => {
+    const response = await supertest(app).get('/api/patients');
+
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body.length).toBeGreaterThan(0)
+  })
+  test('/api/patients/:id returns 200 and nonempty json', async () => {
+    const response = await supertest(app).get('/api/patients/d2773336-f723-11e9-8f0b-362b9e155667');
+
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(Object.keys(response.body).length).toBeGreaterThan(0)
+  })
+  test('/api/patients/:id returns 404', async () => {
+    const response = await supertest(app).get('/api/patients/0');
+
+    expect(response.status).toBe(404);
   })
 })
